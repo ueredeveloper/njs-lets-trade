c6ef1da1fc73be8642b299e70cbdecbdbbfc1746
Fabrício José Barrozo <ueredeveloper@gmail.com>
2024-05-12 18:52:41 -0300

    adicionando novos seletores

--- frontend/view/indicator-view.js
+++ frontend/view/indicator-view.js
@@ -10,29 +10,14 @@ const IndicatorView = {
 
         $(document).ready(function () {
 
-            $('#indicatorType').on('change', function () {
-                let value = $(this).val();
-                /* switch (value) {
-                     case 'movingAverage': IndicatorView.params.push(IndicatorModel.getMovingAverage());
-                         break;
-                     default: IndicatorView.params.push(IndicatorModel.getIchimokuCloud());
-                 }
- 
-                 this.params.forEach(param => {
-                     switch (param.type) {
-                         case 'moving average': $("#indicatorSelects").append(this.renderMovingAverage());
-                             break;
-                         default: $("#indicatorSelects").append(this.renderIchimokuCloud());
-                     }
- 
-                 })*/
 
-            });
 
-            $('#btnSearch').on('click', function () {
+            /*  $('#btnSearch').on('click', function () {
+  
+                  console.log('btn search')
+              });*/
 
-                console.log('btn search')
-            })
+
 
             // Moving Average
 
@@ -160,9 +145,19 @@ const IndicatorView = {
 
         this.div.append(`
             <div class="flex flex-1 flex-col">
-                <div id="indicatorContainer"></div>
+                <div class="indicatorContainer"></div>
                 <div id="btnSearch" class="flex flex-1 justify-end m-2">
-                    <button class="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
+                    <button id="btnPlus" value=0 class="m-2 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+                    </button>
+                    <button id="btnMinus"  class="m-2 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
+                        </svg>
+                    </button>
+                    <button class="m-2 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                         </svg>
@@ -171,102 +166,131 @@ const IndicatorView = {
                 
         `);
 
-        $('#indicatorContainer').append(this.createIndicatorContent(0));
+        $(document).on('change', '.indicatorType', function () {
 
-        
+            let value = $(this).val();
+            let indicatorSelects = $(this).closest('.indicatorContent').find('.indicatorSelects');
 
-        $("#fieldMaIndicator").append(`${this.renderintervals()}`);
-        $("#fieldIchIndicator").append(`${this.renderIchimokuIntervals()}`)
+            console.log(value, indicatorSelects)
+
+            switch (value) {
+                case 'movingAverage':
+                    indicatorSelects.empty();
+                    indicatorSelects.append(IndicatorView.renderMovingAverage());
+                    $("#fieldMaIndicator").append(`${IndicatorView.renderintervals()}`);
+                    break;
+                case 'ichimokuCloud':
+                    indicatorSelects.empty();
+                    indicatorSelects.append(IndicatorView.renderIchimokuCloud());
+                    break;
+                default:
+                    indicatorSelects.empty(); // Clear the content if no option is selected
+            }
+        });
+
+        $('.indicatorContainer').append(this.createIndicatorContent());
+
+
+        $('#btnPlus').on('click', function () {
+            $('.indicatorContainer').append(IndicatorView.createIndicatorContent());
+        });
+
+        $('#btnMinus').on('click', function () {
+            $('.indicatorContainer').children().last().remove();
+        });
+
+
+        //$("#fieldIchIndicator").append(`${this.renderIchimokuIntervals()}`)
 
     },
     renderMovingAverage: function () {
         return `
-         
-              
-              <!-- MA Value - Select -->
-              <select name="length" class="flex-1 mx-2 h-7 " id="maValue">
+            
+            <select name="length" class="flex-1 mx-2 h-7 " id="maValue">
                 <option value="9">09</option>
                 <option value="21">21</option>
                 <option value="200">200</option>
-                      </select>
+            </select>
               
-              <!-- Compare - Select -->
-              <select name="compare" class="flex-1 mx-2 h-7" id="maCompare">
+            <select name="compare" class="flex-1 mx-2 h-7" id="maCompare">
                 <option value="above">Above</option>
                 <option value="bellow">Bellow</option>
-                      </select>
+            </select>
               
-              <!-- Candle - Select -->
-              <select name="candle" class="flex-1 mx-2 h-7" id="maCandle">
+            <select name="candle" class="flex-1 mx-2 h-7" id="maCandle">
                 <option value="high">Candle High</option>
                 <option value="close">Candle Close</option>
                 <option value="low">Candle Low</option>
-              </select>
+            </select>
+            
+            ${this.renderintervals()}
              
               `
     },
     renderIchimokuCloud: function () {
 
         return `
-              <fieldset id="fieldIchIndicator" class="flex flexRow border-2 p-2 items-center">
-                <legend>Ichimoku Cloud</legend>
-                <input type="checkbox" id="ichIndicator" name="ichimokuCloud" value="ichimokuCloud">
-                <label for="ichIndicator" class="mx-2"> Ichimoku Cloud</label>
-                
-                <!-- Line 1 - Select -->
-                <select name="line1" class="flex-1 mx-2 h-7" id="line1">
-                <option value="conversion">Conversion Line</option>
-                <option value="base">Base Line</option>
-                <option value="spanA">Span A</option>
-                <option value="spanB">Span B</option>
-                <option value="spanA+B">Span A and B</option>
-                      </select>
-              
-                <!-- Compare - Select -->
-                <select name="compare" class="flex-1 mx-2 h-7" id="compare">
+             
+            <select name="line1" class="flex-1 mx-2 h-7" id="line1">
+                <option value="conversion" class="bg-green-200">Conversion Line</option>
+                <option value="base" class="bg-green-100">Base Line</option>
+                <option value="spanA" class="bg-green-200">Span A</option>
+                <option value="spanB" class="bg-gray-200">Span B</option>
+                <option value="spanA+B" class="bg-gray-100">Span A and B</option>
+                <option value="high" class="bg-red-200">Candle High</option>
+                <option value="close" class="bg-red-100">Candle Close</option>
+                <option value="low" class="bg-red-200">Candle Low</option>
+            </select>
+            
+            <select name="compare" class="flex-1 mx-2 h-7" id="compare">
                 <option value="above">Above</option>
                 <option value="bellow">Bellow</option>
-                       </select>
-              
-                <!-- Line 2 - Select -->
-                <select name="line2" class="flex-1 mx-2 h-7" id="line2">
-                <option value="conversion">Conversion Line</option>
-                <option value="base">Base Line</option>
-                <option value="spanA">Span A</option>
-                <option value="spanB">Span B</option>
-                <option value="spanA+B">Span A and B</option>
-                </select>
-              
-            </fieldset>
+            </select>
+            
+            <select name="line2" class="flex-1 mx-2 h-7" id="line2">
+                <option value="conversion" class="bg-green-200">Conversion Line</option>
+                <option value="base" class="bg-green-100">Base Line</option>
+                <option value="spanA" class="bg-green-200">Span A</option>
+                <option value="spanB" class="bg-gray-200">Span B</option>
+                <option value="spanA+B" class="bg-gray-100">Span A and B</option>
+                <option value="high" class="bg-red-200">Candle High</option>
+                <option value="close" class="bg-red-100">Candle Close</option>
+                <option value="low" class="bg-red-200">Candle Low</option>
+            </select>
+            
+            ${this.renderintervals()}
+             
         `
     },
 
     renderintervals: function () {
 
         return `
-           
-            <input type="checkbox" id="ma1m" name="1m" value="1h">
-            <label for="ma1m">1m</label><br>
-            <input type="checkbox" id="ma5m" name="5m" value="5m">
-            <label for="ma5m">5m</label><br>
-            <input type="checkbox" id="ma15m" name="15m" value="15m">
-            <label for="ma5m">15m</label><br>
-            <input type="checkbox" id="ma1h" name="1h" value="1h">
-            <label for="ma1h">1h</label><br>
-            <input type="checkbox" id="ma4h" name="4h" value="4h">
-            <label for="ma4h">4h</label><br>
-            <input type="checkbox" id="ma8h" name="8h" value="8h">
-            <label for="ma8h">8h</label><br>
-            <input type="checkbox" id="ma1d" name="1d" value="1d">
-            <label for="ma1d">1d</label><br>
-            <input type="checkbox" id="ma3d" name="3d" value="3d">
-            <label for="ma3d">3d</label><br>
-            <input type="checkbox" id="ma1w" name="1w" value="1w">
-            <label for="ma1w">1w</label><br>
+            <div class="flex flex-row">
+                <input type="checkbox" id="ma1m" name="1m" value="1h">
+                <label for="ma1m" class="mx-1">1m</label><br>
+                <input type="checkbox" id="ma5m name="5m" value="5m">
+                <label for="ma5m" class="mx-1">5m</label><br>
+                <input type="checkbox" id="ma15m" name="15m" value="15m">
+                <label for="ma5m" class="mx-1">15m</label><br>
+                <input type="checkbox" id="ma1h" name="1h" value="1h">
+                <label for="ma1h" class="mx-1">1h</label><br>
+                <input type="checkbox" id="ma4h" name="4h" value="4h">
+                <label for="ma4h" class="mx-1">4h</label><br>
+                <input type="checkbox" id="ma8h" name="8h" value="8h">
+                <label for="ma8h" class="mx-1">8h</label><br>
+                <input type="checkbox" id="ma1d" name="1d" value="1d">
+                <label for="ma1d" class="mx-1">1d</label><br>
+                <input type="checkbox" id="ma3d" name="3d" value="3d">
+                <label for="ma3d" class="mx-1">3d</label><br>
+                <input type="checkbox" id="ma1w" name="1w" value="1w">
+                <label for="ma1w" class="mx-1">1w</label><br>
+            </div>
             
         `
 
     },
+    /*
     renderIchimokuIntervals: function () {
 
         return `
@@ -292,39 +316,23 @@ const IndicatorView = {
           
               `
 
-    },
+    },*/
     createIndicatorButton: function () {
         this.div.append(`<button type="button" class="w-20 m-5 bg-gray-200 hover:bg-green-200 hover:p-0.5 active:bg-blue-200">Search</button>`)
     },
     createIndicatorContent: function (item) {
 
-
         return `
-            <div id="indicatorContent${item}" class="flex w-full items-center bg-green-200 ">
-                <select name="indicatorType" id="indicatorType${item}" class="mx-2 h-7">
+            <div class="indicatorContent flex w-full items-center bg-green-200 p-2">
+                <select name="indicatorType" class="indicatorType" class="mx-2 h-7">
                     <option value="">Indicador</option>
-                    <option value="ichimoku">Ichimoku Cloud</option>
+                    <option value="ichimokuCloud">Ichimoku Cloud</option>
                     <option value="movingAverage">Moving Average</option>
                 </select>
                 <!-- Selects -->
-                <div id="indicatorSelects${item}" class="bg-red-200"></div>
-
-                <!-- Handlers -->
-                <div id="indicatorHandlers${item}" class="flex flex-1 justify-end m-2 bg-red-200">
-                <button id="btnPlus${item}" value=${item} class="m-2 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                    </svg>
-                </button>
-        
-                <button id="btnMinus${item}"  class="m-2 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
-                    </svg>
-                </button>
-                </div>
+                <div class="indicatorSelects flex flex-row" class="bg-red-200"></div>
+               
             </div>
-            
         `
     }
 
