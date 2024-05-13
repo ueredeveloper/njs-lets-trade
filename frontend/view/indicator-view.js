4d31f616bfc09448354ee205f37461344bd764ae
Fabrício José Barrozo <ueredeveloper@gmail.com>
2024-05-13 05:09:44 -0300

    terminando nova view de indicadores

--- frontend/view/indicator-view.js
+++ frontend/view/indicator-view.js
@@ -10,10 +10,13 @@ const IndicatorView = {
 
         $(document).ready(function () {
 
-
+            IndicatorView.params = []
 
             $('#btnSearch').on('click', function () {
-                $('.indicatorContainer').each(function () {
+
+                let params = []
+
+                $('.indicatorContent').each(function () {
                     let container = $(this);
                     let indicatorType = container.find('.indicatorType').val();
                     let selects = container.find('.indicatorSelects select');
@@ -25,125 +28,22 @@ const IndicatorView = {
                         return this.value;
                     }).get();
 
-                    if (indicatorType === 'ichomokuCloud') {
-                        IndicatorView.params.push(`${indicatorType}|${line1}|${compare}|${line2}|${checkboxValues.join('|')}`);
+                    if (indicatorType === 'ichimokuCloud') {
+                        params.push(`${indicatorType}|${line1}|${compare}|${line2}|${checkboxValues.join('|')}`);
 
                     } else if (indicatorType === 'movingAverage') {
-                        IndicatorView.params.push(`${indicatorType}|${line1}|${compare}|${line2}|${checkboxValues.join('|')}`);
+                        params.push(`${indicatorType}|${line1}|${compare}|${line2}|${checkboxValues.join('|')}`);
 
                     } else {
-                        console.log('Indicador não encontrado')
+                        console.log(`Indicador não encontrado: ${indicatorType}`)
                     }
 
-                    console.log(IndicatorView.params)
-                    /* console.log('Line 1:', line1);
-                     console.log('Compare:', compare);
-                     console.log('Line 2:', line2);
-                     console.log('Checkbox Values:', checkboxValues);*/
+                    IndicatorView.params = params;
+
+
                 });
+
             });
-
-
-
-
-            // Moving Average
-
-            /* ['maIndicator', 'ichIndicator'].forEach(item => {
- 
-                 $('#' + item).change(function () {
-                     let name = $(this).attr('name'); // ichimokuCloud movingAverage
- 
-                     console.log(name)
- 
-                     // Seta se checkbox foi clicado ou não
-                     IndicatorView.params[name].checked = $(this).is(':checked');
- 
-                 })
-             });*/
-
-
-
-            /*
- 
-            // ichimokuCloud
- 
-            ['line1', 'compare', 'line2'].forEach(item => {
- 
-                $('#' + item).on('change', function () {
-                    var name = $(this).attr('name');
-                    let value = $(this).val();
-                    let {
-                        ichimokuCloud
-                    } = IndicatorView.params;
- 
-                    ichimokuCloud.checked ? ichimokuCloud[name] = value : ichimokuCloud.params[name] = null
- 
-                    let { line1, compare, line2 } = IndicatorView.params.ichimokuCloud
- 
-                });
- 
-            });
- 
-            ['ich1m', 'ich5m', 'ich15m', 'ich1h', 'ich4h', 'ich8h', 'ich1d', 'ich3d', 'ich1w'].forEach(item => {
- 
-                $('#' + item).change(function () {
-                    let checked = $(this).is(':checked');
-                    let name = $(this).attr('name');
- 
-                    if (checked) {
- 
-                        let ichChecked = IndicatorView.params.ichimokuCloud.checked;
-                        if (ichChecked) {
-                            let intervals = IndicatorView.params.ichimokuCloud.intervals;
- 
-                            intervals.add(name)
-                            console.log('if ich indicator checked', IndicatorView.params.ichimokuCloud.intervals);
-                        }
-                    } else {
- 
-                        console.log('else')
- 
-                        let intervals = IndicatorView.params.ichimokuCloud.intervals;
- 
-                        intervals.delete(name)
-                        console.log('if ich indicator checked', IndicatorView.params.ichimokuCloud.intervals);
- 
- 
-                    }
- 
-                })
-            });
- 
- 
-            ['ma1m', 'ma5m', 'ma15m', 'ma1h', 'ma4h', 'ma8h', 'ma1d', 'ma3d', 'ma1w'].forEach(item => {
- 
-                $('#' + item).change(function () {
-                    let checked = $(this).is(':checked');
-                    let name = $(this).attr('name');
- 
-                    if (checked) {
- 
-                        let maChecked = IndicatorView.params.movingAverage.checked;
-                        if (maChecked) {
-                            let intervals = IndicatorView.params.movingAverage.intervals;
- 
-                            intervals.add(name)
-                            console.log('if ma indicator checked', IndicatorView.params.movingAverage.intervals);
-                        }
-                    } else {
- 
-                        console.log('else')
- 
-                        let intervals = IndicatorView.params.movingAverage.intervals;
- 
-                        intervals.delete(name)
-                        console.log('if ma indicator checked', IndicatorView.params.movingAverage.intervals);
- 
- 
-                    }
- 
-                })
-            });*/
 
         });
 
@@ -205,8 +105,6 @@ const IndicatorView = {
         });
 
 
-        //$("#fieldIchIndicator").append(`${this.renderIchimokuIntervals()}`)
-
     },
     renderMovingAverage: function () {
 
@@ -236,23 +134,24 @@ const IndicatorView = {
 
             });
 
+
         })
         return `
             
             <select name="length" class="flex-1 mx-2 h-7 " id="maLength">
                 <option value="9">09</option>
                 <option value="21">21</option>
-                <option value="200">200</option>
+                <option value="200" selected>200</option>
             </select>
               
             <select name="compare" class="flex-1 mx-2 h-7" id="maCompare">
-                <option value="above">Above</option>
+                <option value="above" selected>Above</option>
                 <option value="bellow">Bellow</option>
             </select>
               
             <select name="candle" class="flex-1 mx-2 h-7" id="maCandle">
                 <option value="high">Candle High</option>
-                <option value="close">Candle Close</option>
+                <option value="close" selected>Candle Close</option>
                 <option value="low">Candle Low</option>
             </select>
             
@@ -265,24 +164,24 @@ const IndicatorView = {
         return `
              
             <select name="line1" class="flex-1 mx-2 h-7" id="line1">
-                <option value="conversion" class="bg-green-200">Conversion Line</option>
+                <option value="conversion" class="bg-green-200" selected>Conversion Line</option>
                 <option value="base" class="bg-green-100">Base Line</option>
                 <option value="spanA" class="bg-green-200">Span A</option>
                 <option value="spanB" class="bg-gray-200">Span B</option>
                 <option value="spanA+B" class="bg-gray-100">Span A and B</option>
                 <option value="high" class="bg-red-200">Candle High</option>
                 <option value="close" class="bg-red-100">Candle Close</option>
-                <option value="low" class="bg-red-200">Candle Low</option>
+            <option value="low" class="bg-red-200">Candle Low</option>
             </select>
             
             <select name="compare" class="flex-1 mx-2 h-7" id="compare">
-                <option value="above">Above</option>
-                <option value="bellow">Bellow</option>
+                <option value="above" selected>Above</option>
+                <option value="bellow">Below</option>
             </select>
             
             <select name="line2" class="flex-1 mx-2 h-7" id="line2">
                 <option value="conversion" class="bg-green-200">Conversion Line</option>
-                <option value="base" class="bg-green-100">Base Line</option>
+                <option value="base" class="bg-green-100" selected>Base Line</option>
                 <option value="spanA" class="bg-green-200">Span A</option>
                 <option value="spanB" class="bg-gray-200">Span B</option>
                 <option value="spanA+B" class="bg-gray-100">Span A and B</option>
@@ -290,6 +189,7 @@ const IndicatorView = {
                 <option value="close" class="bg-red-100">Candle Close</option>
                 <option value="low" class="bg-red-200">Candle Low</option>
             </select>
+    
             
             ${this.renderintervals()}
              
@@ -306,7 +206,7 @@ const IndicatorView = {
                 <label for="ma5m" class="mx-1">5m</label><br>
                 <input type="checkbox" id="ma15m" name="15m" value="15m">
                 <label for="ma5m" class="mx-1">15m</label><br>
-                <input type="checkbox" id="ma1h" name="1h" value="1h">
+                <input type="checkbox" id="ma1h" name="1h" value="1h" checked>
                 <label for="ma1h" class="mx-1">1h</label><br>
                 <input type="checkbox" id="ma4h" name="4h" value="4h">
                 <label for="ma4h" class="mx-1">4h</label><br>
@@ -323,33 +223,7 @@ const IndicatorView = {
         `
 
     },
-    /*
-    renderIchimokuIntervals: function () {
 
-        return `
-        
-          <input type="checkbox" id="ich1m" name="1m" value="1h">
-          <label for="ich1m">1m</label><br>
-          <input type="checkbox" id="ich5m" name="5m" value="5m">
-          <label for="ich5m">5m</label><br>
-          <input type="checkbox" id="ich15m" name="15m" value="15m">
-          <label for="ich5m">15m</label><br>
-          <input type="checkbox" id="ich1h" name="1h" value="1h">
-          <label for="ich1h">1h</label><br>
-          <input type="checkbox" id="ich4h" name="4h" value="4h">
-          <label for="ich4h">4h</label><br>
-          <input type="checkbox" id="ch8h" name="8h" value="8h">
-          <label for="ch8h">8h</label><br>
-          <input type="checkbox" id="ich1d" name="1d" value="1d">
-          <label for="ich1d">1d</label><br>
-          <input type="checkbox" id="ch3d" name="3d" value="3d">
-          <label for="ch3d">3d</label><br>
-          <input type="checkbox" id="ich1w" name="1w" value="1w">
-          <label for="ich1w">1w</label><br>
-          
-              `
-
-    },*/
     createIndicatorButton: function () {
         this.div.append(`<button type="button" class="w-20 m-5 bg-gray-200 hover:bg-green-200 hover:p-0.5 active:bg-blue-200">Search</button>`)
     },
@@ -368,7 +242,6 @@ const IndicatorView = {
             </div>
         `
     }
-
 
 }
 
